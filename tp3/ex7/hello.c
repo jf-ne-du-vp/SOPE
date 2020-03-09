41dc4d07cbf3d4eb81ee91cd50b4776e1e2abf1f
Joao Nuno Diegues Vasconcelos <up201504397@fe.up.pt>
2020-03-09 19:36:43 +0000

    started 7

---
+++ tp3/ex7/hello.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void){
+    printf("Just to see if exclp is working, HELLO!!");
+
+    return 0;
+}
--- tp3/ex7/p7.c
+++ tp3/ex7/p7.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[]){
     char prog[20];
 
-    sprintf(prog, "%s.c", argv[1]);
+    sprintf(prog, "%s.c", argv[1]);  //first argument is stored string prog
     execlp("gcc", "gcc", prog, "-Wall", "-o", argv[1], NULL);
     printf("Error changing code");
-    exit(1);
+    exit(-1);
 }
