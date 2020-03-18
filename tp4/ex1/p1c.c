d551d52ec7ef2b1086e344ff1685d20b1d9da9e6
jf-ne-du-vp <up201504397@fe.up.pt>
2020-03-18 17:39:46 +0000

    finished 1 started 2

--- tp4/ex1/p1c.c
+++ tp4/ex1/p1c.c
@@ -9,8 +9,14 @@ void sigint_handler(int signo){
 
 int main(void){
     unsigned int timeLeft = 30;
+    struct sigaction act;
+    act.sa_flags = 0;
+    act.sa_handler = sigint_handler;            //what handler to use
 
-    if(signal(SIGINT, sigint_handler) == SIG_ERR){
+    sigemptyset(&act.sa_mask);
+
+
+    if(sigaction(SIGINT, &act, NULL) < 0){
         fprintf(stderr, "Unable to install SIGINT handler\n");
         exit(1);
     }
